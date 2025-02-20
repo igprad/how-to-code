7919f943f63fb6ae40c4b42dc07c0064caf042f8
igprad <ign.aldi.pradana@gmail.com>
2025-02-20 17:32:24 +0700

    chore: update readme also try to change the thread and block kernel execution using args

--- parallel-things/cpp-cuda/README.md
+++ parallel-things/cpp-cuda/README.md
@@ -4,22 +4,37 @@ This is an example utilize nvidia to processing all values in the csv async.\
 What is CUDA? Here: https://en.wikipedia.org/wiki/CUDA \
 In this example program, will compare the sumarization result between `cpp` and `cuda`.
 
-## Pre-req
-Installed `nvcc` and `cuda utilities` https://developer.nvidia.com/cuda-downloads, also gcc.\
-If you're using windows. You can utilize Visual Studio.\
-Example dataset provided here (1 mil rows that containt 3 columns of float numbers)
+## Requirements
+- Installed C++ compiler.
+- Installed `nvcc` and `cuda utilities` https://developer.nvidia.com/cuda-downloads, also gcc.
+If you're using windows. You can utilize Visual Studio.
+This is my setups:
+```
+gcc version 14.2.1 20250207 (GCC)
 
-## How-to
-If you're using native compiler `nvcc` and `gcc` like me. You can compile and run it. Using default: \
-`cpp` -> `gcc sum.cpp -o cpp.out` \
-`cuda` -> `nvcc sum.cu -o cuda.out` \
-then run the shells to getting the result on your local machine.
+nvcc: NVIDIA (R) Cuda compiler driver
+Copyright (c) 2005-2025 NVIDIA Corporation
+Built on Wed_Jan_15_19:20:09_PST_2025
+Cuda compilation tools, release 12.8, V12.8.61
+Build cuda_12.8.r12.8/compiler.35404655_0
+```
+
+## Dataset
+Example dataset provided here is `dataset.csv` that contains 1 mil rows data with 3 columns of float numbers) \
+The example program will sum all the data for each columns first then later on will sum all the results.
+
+## How to
+If you're using native compiler `nvcc` and `g++`, you can compile and run the program with default commands:
+```
+`cpp` -> `g++ sum.cpp -o cpp.out && ./cpp.out` \
+`cuda` -> `nvcc -Wno-deprecated-gpu-targets sum.cu -o cuda.out && ./cuda.out <desired thread> <desired block>` \ 
+```
 
 ## Result
 The result varies, but almost consistent on my local machine for the difference.\
-The `cuda` program got almost +- 15 times faster.\
-When this README updated, the `cpp` got result +- `6ms` and the `cuda` got `0.5ms`\
-The sumarization result also correct.
+The `cuda` program should be faster.\
 
-## My Spec
-Running on NVIDIA-1070Ti x i7-4ish :(
+## My Result
+My result in my local machine (i5-12600K && RTX 3060 (12GB)) run on Arch.
+- cpp -> result: 1.49936e+06 with time cost ~ 2.55431 ms.
+- cuda -> result: 1.49936e+06 with time cost ~ 0.310688 ms. 
--- parallel-things/cpp-cuda/sum.cu
+++ parallel-things/cpp-cuda/sum.cu
@@ -1,116 +1,135 @@
-#include <iostream>
+#include <exception>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <chrono>
 
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 
-#define MAX 1000000
-
+#define DEFAULT_MAX_ROWS 1000000
 
 __device__ double sum(double first_val, double second_val, double third_val) {
-    return first_val + second_val + third_val;
+  return first_val + second_val + third_val;
 }
 
-__global__ void sum_csv_data_with_cuda(double *result, double *first_row, double *second_row, double *third_row, int max) {
-    int offset_x = threadIdx.x;
-    int offset_y = blockIdx.x;
-    int offset = offset_y + offset_x * max;
-    if (offset < MAX) result[offset] = sum(first_row[offset], second_row[offset], third_row[offset]);
+__global__ void sum_csv_data_with_cuda(double *result, double *first_col,
+                                       double *second_col, double *third_col,
+                                       int max) {
+  int offset_x = threadIdx.x;
+  int offset_y = blockIdx.x;
+  int offset = offset_y + offset_x * max;
+  if (offset < DEFAULT_MAX_ROWS)
+    result[offset] =
+        sum(first_col[offset], second_col[offset], third_col[offset]);
 }
 
-int main() {
-    std::ifstream csv ("dataset.csv");
-    double* first_value = (double*) malloc(sizeof(double) * MAX);
-    double* second_value = (double*) malloc(sizeof(double) * MAX);
-    double* third_value = (double*) malloc(sizeof(double) * MAX);
-    double* result = (double*)malloc(sizeof(double) * MAX);
-
-    // Getting the value from dataset
-    std::string value;
-    int idx = 0;
-    bool csv_title_skipped = false;
-    while (std::getline(csv, value)) {
-        if (!csv_title_skipped) {
-            csv_title_skipped = true;
-            continue;
-        }
-        std::string number;
-        std::stringstream lineToInputBuffered(value);
-        std::string::size_type sz;
-        int rowIdx = 0;
-        while (std::getline(lineToInputBuffered, number, ',')) {
-            try {
-                if (rowIdx == 1)
-                   first_value[idx] = std::stod(number, &sz);
-                if (rowIdx == 2)
-                   second_value[idx] = std::stod(number, &sz);
-                if (rowIdx == 3)
-                   third_value[idx] = std::stod(number, &sz);
-            } catch (const std::exception& e) {
-                std::cerr << "error: "<< e.what() << std::endl;
-            }
-            rowIdx++;
-        }
-        idx++;
-    }
-
-    /*
-     * Will calculate the time spent to sum all values for each rows.
-     * This section will be quite different with `sum.cpp`,
-     * since will use cuda api to do the parallel tasks.
-     * */
-    double* first_dev = (double*) malloc(sizeof(double) * MAX);
-    cudaMalloc(&first_dev, sizeof(double) * MAX);
-    cudaMemcpy(first_dev, first_value, sizeof(double) * MAX, cudaMemcpyHostToDevice);
-
-    double* second_dev = (double*) malloc(sizeof(double) * MAX);
-    cudaMalloc(&second_dev, sizeof(double) * MAX);
-    cudaMemcpy(second_dev, second_value, sizeof(double) * MAX, cudaMemcpyHostToDevice);
-
-    double* third_dev = (double*) malloc(sizeof(double) * MAX);
-    cudaMalloc(&third_dev, sizeof(double) * MAX);
-    cudaMemcpy(third_dev, third_value, sizeof(double) * MAX, cudaMemcpyHostToDevice);
-    
-    double* result_dev = (double*) malloc(sizeof(double) * MAX);
-    cudaMalloc(&result_dev, sizeof(double) * MAX);
-
-    cudaEvent_t start, stop;
-    float time = 0;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-
-    cudaEventRecord(start, 0);
-    sum_csv_data_with_cuda<<<1000, 1000>>>(result_dev, first_dev, second_dev, third_dev, 1000); 
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&time, start, stop);
-
-    cudaMemcpy(result, result_dev, sizeof(double) * MAX, cudaMemcpyDeviceToHost);
-
-    cudaFree(first_dev);
-    cudaFree(second_dev);
-    cudaFree(third_dev);
-    cudaFree(result_dev);
-    cudaFree(&start);
-    cudaFree(&stop);
-    /*
-     * end section
-     */
-
-    double total = 0;
-    for(int i=0; i < MAX; i++) {
-        total+=result[i];
-    }
-    std::cout<<"Total sum of all rows = " << total << std::endl;
-    std::cout<<"With elapsed time while using cuda threads (ms) = " << time << std::endl;
-
-    free(first_value);
-    free(second_value);
-    free(third_value);
-    free(result);
+int main(int argc, char **argv) {
+  // parse args
+  int thread_in, block_in;
+  try {
+    thread_in = std::stoi(argv[1]);
+    block_in = std::stoi(argv[2]);
+  } catch (const std::exception &e) {
+    std::cerr << "Invalid arguments. Please only pass the correct thread and "
+                 "block. e.g. 1000 1000";
     return 0;
+  }
+
+  std::ifstream csv("dataset.csv");
+  double *first_value = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  double *second_value = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  double *third_value = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  double *result = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+
+  // Getting the value from dataset
+  std::string value;
+  int idx = 0;
+  bool csv_title_skipped = false;
+  while (std::getline(csv, value)) {
+    if (!csv_title_skipped) {
+      csv_title_skipped = true;
+      continue;
+    }
+    std::string number;
+    std::stringstream lineToInputBuffered(value);
+    std::string::size_type sz;
+    int colIdx = 0;
+    while (std::getline(lineToInputBuffered, number, ',')) {
+      try {
+        if (colIdx == 1)
+          first_value[idx] = std::stod(number, &sz);
+        if (colIdx == 2)
+          second_value[idx] = std::stod(number, &sz);
+        if (colIdx == 3)
+          third_value[idx] = std::stod(number, &sz);
+      } catch (const std::exception &e) {
+        std::cerr << "error: " << e.what() << std::endl;
+      }
+      colIdx++;
+    }
+    idx++;
+  }
+
+  /*
+   * Will calculate the time spent to sum all values for each rows.
+   * This section will be quite different with `sum.cpp`,
+   * since will use cuda api to do the parallel tasks.
+   * */
+  double *first_dev = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMalloc(&first_dev, sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMemcpy(first_dev, first_value, sizeof(double) * DEFAULT_MAX_ROWS,
+             cudaMemcpyHostToDevice);
+
+  double *second_dev = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMalloc(&second_dev, sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMemcpy(second_dev, second_value, sizeof(double) * DEFAULT_MAX_ROWS,
+             cudaMemcpyHostToDevice);
+
+  double *third_dev = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMalloc(&third_dev, sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMemcpy(third_dev, third_value, sizeof(double) * DEFAULT_MAX_ROWS,
+             cudaMemcpyHostToDevice);
+
+  double *result_dev = (double *)malloc(sizeof(double) * DEFAULT_MAX_ROWS);
+  cudaMalloc(&result_dev, sizeof(double) * DEFAULT_MAX_ROWS);
+
+  cudaEvent_t start, stop;
+  float time = 0;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
+  cudaEventRecord(start, 0);
+  sum_csv_data_with_cuda<<<thread_in, block_in>>>(
+      result_dev, first_dev, second_dev, third_dev, block_in);
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&time, start, stop);
+
+  cudaMemcpy(result, result_dev, sizeof(double) * DEFAULT_MAX_ROWS,
+             cudaMemcpyDeviceToHost);
+
+  cudaFree(first_dev);
+  cudaFree(second_dev);
+  cudaFree(third_dev);
+  cudaFree(result_dev);
+  cudaFree(&start);
+  cudaFree(&stop);
+  /*
+   * end section
+   */
+
+  double total = 0;
+  for (int i = 0; i < DEFAULT_MAX_ROWS; i++) {
+    total += result[i];
+  }
+  std::cout << "Total sum of all rows = " << total << std::endl;
+  std::cout << "With elapsed time while using cuda threads (ms) = " << time
+            << std::endl;
+
+  free(first_value);
+  free(second_value);
+  free(third_value);
+  free(result);
 }
